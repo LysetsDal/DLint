075498eba9870ff317b94ff5a3d3a88eb409e056
LysetsDal <asly@itu.dk>
2024-05-07 16:03:17 +0200

    SHB109: more fitting description

--- Linterd/Rules/Bash/SHB109.fs
+++ Linterd/Rules/Bash/SHB109.fs
@@ -5,5 +5,5 @@ open Rules.ShellWarn
 let shb109 : BinWarn = {
     ErrorCode = "SHB109"
     Binary = "mount"
-    ErrorMsg = "Running a standard mount in a RUN command is impractical due to namespace isolation and the image-based filesystem docker uses. Instead mount filesystems through volumes, or from the Docker specific 'RUN --mount=type...' command."
+    ErrorMsg = "Running a 'standard' mount in a RUN command should be avoided. Instead mount filesystems through volumes, or from the Docker specific 'RUN --mount=type...' command."
 }
