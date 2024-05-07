dbd84e70dd2cc514d249f47346ff7a9a46c3fcd0
LysetsDal <asly@itu.dk>
2024-05-07 16:03:43 +0200

    Added documentation link to SHB111

--- Linterd/Rules/Bash/SHB111.fs
+++ Linterd/Rules/Bash/SHB111.fs
@@ -2,6 +2,7 @@ module Rules.Bash.SHB111
 
 open Rules.ShellWarn
 
+// https://docs.docker.com/config/containers/multi-service_container/
 let shb111 : BinWarn = {
     ErrorCode = "SHB111"
     Binary = "service"
