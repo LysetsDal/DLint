217225bfd56668e384b635210b0bdb23902bb029
LysetsDal <asly@itu.dk>
2024-05-13 19:05:07 +0200

    Added space.

--- DLint/Rules/Mounts/Mnt107.fs
+++ DLint/Rules/Mounts/Mnt107.fs
@@ -5,5 +5,5 @@ open Rules.MountWarn
 let mnt107 : SensitiveMount = {
     ErrorCode = "MNTW107"
     MountPoint = "/proc/mem"
-    ErrorMsg = "Interfaces with the kernel memory device /dev/mem. Historically vulnerable to privilege escalation attacks.This file can be used to access the pages of a process's memory through open(2), read(2), and lseek(2)."
+    ErrorMsg = "Interfaces with the kernel memory device /dev/mem. Historically vulnerable to privilege escalation attacks. This file can be used to access the pages of a process's memory through open(2), read(2), and lseek(2)."
 }
